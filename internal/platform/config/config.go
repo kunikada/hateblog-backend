107a4ef0a53b4f6d546affb947a396b72f1c1aa5
kunikada <kunikada@gmail.com>
2026-01-08 07:02:18 +0000

    環境変数名の変更

--- internal/platform/config/config.go
+++ internal/platform/config/config.go
@@ -98,7 +98,7 @@ type AppConfig struct {
 	EnableMetrics   bool          `env:"APP_ENABLE_METRICS" envDefault:"true"`
 	APIBasePath     string        `env:"APP_API_BASE_PATH" envDefault:"/api/v1"`
 	APIKeyRequired  bool          `env:"APP_API_KEY_REQUIRED" envDefault:"false"`
-	APIKeyPrefix    string        `env:"API_KEY_PREFIX" envDefault:"hb_live_"`
+	APIKeyPrefix    string        `env:"APP_API_KEY_PREFIX" envDefault:"hb_live_"`
 	APIKeyTTL       time.Duration `env:"APP_API_KEY_TTL" envDefault:"0"`
 
 	RateLimitEnabled     bool          `env:"APP_RATE_LIMIT_ENABLED" envDefault:"false"`
