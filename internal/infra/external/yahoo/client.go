1028592070b78d00b307d3a9e2a08b6fdf088461
kunikada <kunikada@gmail.com>
2026-01-19 08:32:22 +0000

    User-AgentヘッダーにYahoo AppIDを追加して、リクエスト情報を強化

--- internal/infra/external/yahoo/client.go
+++ internal/infra/external/yahoo/client.go
@@ -89,8 +89,7 @@ func (c *Client) Extract(ctx context.Context, text string) ([]Keyphrase, error) 
 		return nil, err
 	}
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("User-Agent", c.userAgent)
-	req.Header.Set("X-Yahoo-App-Id", c.appID)
+	req.Header.Set("User-Agent", fmt.Sprintf("%s; Yahoo AppID: %s", c.userAgent, c.appID))
 
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
