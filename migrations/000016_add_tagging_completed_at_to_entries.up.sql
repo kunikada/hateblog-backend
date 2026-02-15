853f265a6fed051c44c4351699ba316c71d5aade
kunikada <kunikada@gmail.com>
2026-02-15 03:57:46 +0000

    feat: simplify tagging_completed_at update logic for entries to use created_at directly

--- migrations/000016_add_tagging_completed_at_to_entries.up.sql
+++ migrations/000016_add_tagging_completed_at_to_entries.up.sql
@@ -4,13 +4,7 @@ ADD COLUMN IF NOT EXISTS tagging_completed_at TIMESTAMP WITH TIME ZONE;
 COMMENT ON COLUMN entries.tagging_completed_at IS 'Yahooタグ付け処理の完了日時（タグ0件を含む）';
 
 UPDATE entries e
-SET tagging_completed_at = e.created_at
-FROM (
-    SELECT DISTINCT entry_id
-    FROM entry_tags
-) et
-WHERE et.entry_id = e.id
-AND e.tagging_completed_at IS NULL;
+SET tagging_completed_at = e.created_at;
 
 CREATE INDEX IF NOT EXISTS idx_entries_tagging_pending_created_at
 ON entries (created_at DESC)
